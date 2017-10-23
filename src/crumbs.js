135ee4bf9ffb7d3c6101a049d385710fd01c3048
Nicholas Dunkel <nicholasdunkel@NicholasDunkel.local>
2017-10-23 22:53:40 +0200

    added sunboxlabs

--- src/crumbs.js
+++ src/crumbs.js
@@ -56,85 +56,85 @@ export default class Crumbs extends React.Component {
           <ol>
 
             <li>
-              <a href="/electricfeed/2017/06/20/200-apartment-solar-system.html">
+              <a target="_blank" href="/electricfeed/2017/06/20/200-apartment-solar-system.html">
               Project: $200 Off-Grid Solar For Apartments 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/09/06/burning-man-update.html">
+              <a target="_blank" href="/electricfeed/2017/09/06/burning-man-update.html">
               Project: Burning Man System 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/09/17/what-will-it-run.html">
+              <a target="_blank" href="/electricfeed/2017/09/17/what-will-it-run.html">
               Project: Low-energy devices for an off-grid home 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/09/30/box-update.html">
+              <a target="_blank" href="/electricfeed/2017/09/30/box-update.html">
               Project: Box Update 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/01/burning-man-solar-2.html">
+              <a target="_blank" href="/electricfeed/2017/10/01/burning-man-solar-2.html">
               Project: For Burning Man / Camping (Lithium battery) 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/07/solarpi.html">
+              <a target="_blank" href="/electricfeed/2017/10/07/solarpi.html">
               Project: SolarPi 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/07/stock-sunjack-in-window.html">
+              <a target="_blank" href="/electricfeed/2017/10/07/stock-sunjack-in-window.html">
               Project: sunjack stock in window 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/09/hurricane.html">
+              <a target="_blank" href="/electricfeed/2017/10/09/hurricane.html">
               Project: Hurricane Solar Lighting 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/10/build-it.html">
+              <a target="_blank" href="/electricfeed/2017/10/10/build-it.html">
               Link: More than 500 renewable energy and conservation projects you can build 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/10/check-reddit.html">
+              <a target="_blank" href="/electricfeed/2017/10/10/check-reddit.html">
               Link: The Reddit DIY Community 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/10/desert-power.html">
+              <a target="_blank" href="/electricfeed/2017/10/10/desert-power.html">
               Link: Desert power (without lots of generators) 
               </a>&nbsp;
             </li>
             
             <li> 
-              <a href="/electricfeed/2017/10/10/full-rv.html">
+              <a target="_blank" href="/electricfeed/2017/10/10/full-rv.html">
               Link: Full RV Solar &amp; Inverter Install ~ From Start To Finish (Video) 
               </a>&nbsp;
             </li>
             
             <li>
-              <a href="/electricfeed/2017/10/10/hackaday-solar.html">
+              <a target="_blank" href="/electricfeed/2017/10/10/hackaday-solar.html">
               Link: Hackaday Solar Projects 
               </a>&nbsp;
             </li>
             
             <li> 
-              <a href="/electricfeed/2017/10/16/solar-smartwatch.html">
+              <a target="_blank" href="/electricfeed/2017/10/16/solar-smartwatch.html">
               Link: LunaR, the world’s first solar powered smartwatch.  
               </a>&nbsp;
             </li>

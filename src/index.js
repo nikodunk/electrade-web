e2c3d37df5b5db9d4695ad83cf65e75e7560b9de
Nikolai <n.dunkel@gmail.com>
2019-02-18 11:58:59 -0800

    rebuilding site Mon Feb 18 11:58:59 PST 2019

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import React from 'react';
-import { render } from 'react-snapshot';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+import React from 'react';
+import { hydrate, render } from "react-dom";
+import App from './App';
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+

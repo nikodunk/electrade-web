3dca55f026a0ae7b7dab7dbed7cb1ee559728f5a
Nikolai <n.dunkel@gmail.com>
2019-02-18 11:26:27 -0800

    rebuilding site Mon Feb 18 11:26:27 PST 2019

---
+++ src/Crumbs/ListingCrumb.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+export default class ListingCrumb extends React.Component {
+
+
+  render() {
+    return (
+      <div class="box" id="theForm">
+        
+        <p><b>List your Used or New EV here too:</b></p>
+        <form action="https://formspree.io/niko@sunboxlabs.com"
+              method="POST"
+              id="formspree">
+            <input type="text" placeholder="Your Car" name="name" />&nbsp;
+            <input type="email" placeholder="your email" name="_replyto" />&nbsp;
+            <button type="submit" value="Submit"><b>+ Submit</b></button>
+        </form>
+
+        {/*<form>
+              <input
+                id="myInput"
+                type="text"
+                name="email"
+                placeholder="youremail@example.com"
+              />&nbsp;
+              <button onClick={this.handleSubmit}>submit</button>
+        </form>*/}
+      </div>
+    )
+  }
+}
+
+
+
--- src/crumbs.js
+++ src/crumbs.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PriceTrackerUsed from './Crumbs/PriceTrackerUsed'
 import Guides from './Crumbs/Guides'
+import ListingCrumb from './Crumbs/ListingCrumb'
 
 
 export default class Crumbs extends React.Component {
@@ -40,26 +41,8 @@ export default class Crumbs extends React.Component {
   render() {
     return (
       <div>
-        <div class="box" id="theForm">
-          
-          <p><b>List your Used or New EV here too:</b></p>
-          <form action="https://formspree.io/n.dunkel@gmail.com"
-                method="POST"
-                id="formspree">
-              <input type="text" placeholder="Your Car" name="name" />&nbsp;
-              <input type="email" placeholder="your email" name="_replyto" />&nbsp;
-              <button type="submit" value="Submit"><b>+ Submit</b></button>
-          </form>
-
-          {/*<form>
-                <input
-                  id="myInput"
-                  type="text"
-                  name="email"
-                  placeholder="youremail@example.com"
-                />&nbsp;
-                <button onClick={this.handleSubmit}>submit</button>
-          </form>*/}
+        <div>
+          <ListingCrumb />
         </div>
 
         <div>

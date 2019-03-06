a1bbd8dc9a967474e93225c2d1f867d89b50f25b
Nikolai <n.dunkel@gmail.com>
2019-03-06 14:32:47 -0800

    Updates

---
+++ precache-manifest.08f25465177061b98c27cb630fbfbe71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8b8ac27d7f593116f8d5",
+    "url": "/static/js/main.8b8ac27d.chunk.js"
+  },
+  {
+    "revision": "94302b8b28fbefe15ce1",
+    "url": "/static/js/2.94302b8b.chunk.js"
+  },
+  {
+    "revision": "8b8ac27d7f593116f8d5",
+    "url": "/static/css/main.945d9045.chunk.css"
+  },
+  {
+    "revision": "c04e9a017425afc50a0f6b1df9e62847",
+    "url": "/index.html"
+  }
+];

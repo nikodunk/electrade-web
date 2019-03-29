0828ec084d001f26772e7945cbff6d018c970a68
Nikolai <n.dunkel@gmail.com>
2019-03-29 10:51:22 -0700

    rebuilding site Fri Mar 29 10:51:22 PDT 2019

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ export default class App extends React.Component{
               </div>
 
               <div class="listings">
-                <PriceTracker />
+                
               </div>
 
             </div>

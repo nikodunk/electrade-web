e3f90ff9551fb347cd3a6a030f5dc8972b050adf
Nikolai <nikolai@Nikolais-MacBook-Air.local>
2019-09-03 09:58:46 -0700

    Updates

---
+++ precache-manifest.629f22d582f8c61c4049bb233bae9d4b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef0a743e0d2f10ec41f81cbbf952ec2",
+    "url": "/electrify-web/static/media/etron.0ef0a743.jpg"
+  },
+  {
+    "revision": "2f611907cdb89c832368",
+    "url": "/electrify-web/static/css/main.c64bddda.chunk.css"
+  },
+  {
+    "revision": "b5df71a4ed4f7e9d53db",
+    "url": "/electrify-web/static/js/runtime~main.b5df71a4.js"
+  },
+  {
+    "revision": "979bcf57c1c1779d1e38",
+    "url": "/electrify-web/static/js/2.979bcf57.chunk.js"
+  },
+  {
+    "revision": "2b3bb081775b76b02a04768353c85642",
+    "url": "/electrify-web/static/media/model3.2b3bb081.jpg"
+  },
+  {
+    "revision": "3b938641c3c8a006a24f1d0b6a5f97a2",
+    "url": "/electrify-web/static/media/bolt.3b938641.jpg"
+  },
+  {
+    "revision": "ce7844ba4fa4784fae8dc69bc9dde679",
+    "url": "/electrify-web/static/media/leaf.ce7844ba.jpg"
+  },
+  {
+    "revision": "91ed75b9944ab0961e74a9c9a39ece71",
+    "url": "/electrify-web/static/media/kona.91ed75b9.jpg"
+  },
+  {
+    "revision": "2f611907cdb89c832368",
+    "url": "/electrify-web/static/js/main.2f611907.chunk.js"
+  },
+  {
+    "revision": "4b334f0efcc8e6eb57ba9a6d8a459b4b",
+    "url": "/electrify-web/static/media/i3.4b334f0e.jpg"
+  },
+  {
+    "revision": "61a9a2231a61e7b21e6e4e825f16cfbb",
+    "url": "/electrify-web/static/media/fiat.61a9a223.jpg"
+  },
+  {
+    "revision": "f41734f717442db7aadf0f613c04f0a0",
+    "url": "/electrify-web/static/media/golf.f41734f7.jpg"
+  },
+  {
+    "revision": "f4e6f27620454ede320e6c8410f7d9e5",
+    "url": "/electrify-web/static/media/bmw330e.f4e6f276.jpg"
+  },
+  {
+    "revision": "6b50995d488b3a068e2dcc3025529e86",
+    "url": "/electrify-web/static/media/prime.6b50995d.jpg"
+  },
+  {
+    "revision": "3a3e84021e727536536fd533d15bcaff",
+    "url": "/electrify-web/static/media/volt.3a3e8402.jpg"
+  },
+  {
+    "revision": "7d919d46e67a65beb21df4e23cd36a8d",
+    "url": "/electrify-web/static/media/niro.7d919d46.jpg"
+  },
+  {
+    "revision": "6a7a7335f95bc82479286224eae76ad6",
+    "url": "/electrify-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37f76ca135723a3417b552931895dc3d.js"
+  "/electrify-web/precache-manifest.629f22d582f8c61c4049bb233bae9d4b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/electrify-web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
